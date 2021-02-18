c076819d282f4b0058907e464735cf2333939430
Florian Blandin <pro.blandinf@gmail.com>
2021-02-18 10:29:15 +0100

    exo2

---
+++ app/src/main/java/com/blandinf/exampledmii/SecondActivity.java
@@ -0,0 +1,33 @@
+package com.blandinf.exampledmii;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.blandinf.exampledmii.databinding.ActivitySecondBinding;
+
+public class SecondActivity extends AppCompatActivity {
+
+    ActivitySecondBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySecondBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.resultButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (binding.firstNumber != null && binding.secondNumber != null) {
+                    Log.v("teeest", binding.firstNumber.getText().toString());
+                    Log.v("teeest", binding.firstNumber.toString());
+                    Integer result = Integer.parseInt(binding.firstNumber.getText().toString()) + Integer.parseInt(binding.secondNumber.getText().toString());
+                    binding.result.setText(result.toString());
+                }
+            }
+        });
+    }
+}
